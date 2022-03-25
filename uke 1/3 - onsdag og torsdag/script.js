fe30c91f30c6abf8478c2e62a3995b3b2a1f09aa
Srimmy <srpra001@osloskolen.no>
2022-03-25 11:29:09 +0100

    fredag uke 1

--- uke 1/3 - onsdag og torsdag/script.js
+++ uke 1/3 - onsdag og torsdag/script.js
@@ -20,13 +20,7 @@ for (i of document.getElementsByTagName('input')) {
     console.log(i);
 }
 
-//funksjon som legger alle feltene på nettsiden
-function printInput(tittel, array, name, inputType) {
-    inputDiv.innerHTML += "<h3> " + tittel + "</h3>";
-    for (let service of array) {
-        inputDiv.innerHTML += "<input name = '" + name + "' type = '" + inputType + "' readonly value = '" + service.navn + "|" + service.pris + "' > <label>" + service.navn + "</label> <br>";
-    }
-}
+
 
 form.addEventListener("submit", function (e) {
 
@@ -41,14 +35,16 @@ form.addEventListener("submit", function (e) {
     let minusARabatt = 0;
     let minusERabatt = 0;
 
-    for (let valg of formEl.engangskost) {
-        if (valg.checked) {
+    for (let valg of formEl.engangskost) { //ser hvilke checkboxer som er krysset av
+        if (valg.checked) { //kjører kode hvis en checkbox er huket av
+            //har 2 verdier i valie attribute, de kan deles opp med "|"
             let splitValue = valg.value.split("|");
+            //gjør verdier til objekter i en array
             valgteEngagskoster.push({ navn: splitValue[0], pris: splitValue[1] });
         }
     }
     if (valgtAbonoment[0] || valgteEngagskoster[0]) {
-        console.log(formEl.kunde.value, formEl.agent.value, valgtAbonoment[1]);
+        //case 1: enten en radio knapp eller checkbox er krysset av
         output.innerHTML = 'Hei ' + formEl.kunde.value + ' og takk for en hyggelig telefonsamtale. \n\n';
         aRabattString = "";
         eRabattString = "";
@@ -57,6 +53,7 @@ form.addEventListener("submit", function (e) {
         //rabatter
         switch (true) {
             case form.abonomentRabatt.value != '' && form.engangskostRabatt.value != '':
+                //case 1: det er rabatt på abonoment og engangskost
                 output.innerHTML += "Sender deg som avtalt " + form.engangskostRabatt.value + "% tilbud på engangskost produktene og " + form.abonomentRabatt.value + "% tilbud på abonomentet \n\n";
                 aRabatt -= parseFloat(form.abonomentRabatt.value) / 100;
                 eRabatt -= parseFloat(form.engangskostRabatt.value) / 100;
@@ -68,6 +65,7 @@ form.addEventListener("submit", function (e) {
 
                 break;
             case form.abonomentRabatt.value != '':
+                //case 2: det er rabatt på abonoment
                 output.innerHTML += "Sender deg som avtalt " + form.abonomentRabatt.value + "% tilbud på abonomentet \n\n";
                 aRabatt -= parseFloat(form.abonomentRabatt.value) / 100;
 
@@ -75,50 +73,62 @@ form.addEventListener("submit", function (e) {
                 aRabattString = "Rabatt: kr " + minusARabatt + ",-";
                 break;
             case form.engangskostRabatt.value != '':
+                //case 3: det er rabatt på engangskost
                 output.innerHTML += "Sender deg som avtalt " + form.engangskostRabatt.value + "% tilbud på engangskost produktene \n\n";
                 eRabatt -= parseFloat(form.engangskostRabatt.value) / 100;
                 break;
             default:
+                //case 4: det er ingen rabatt
                 output.innerHTML += "Sender deg som avtalt tilbud på ingen produkter fordi vi gir ikke rabatt \n\n"
                 break;
         }
+        //skriver ut regning
         output.innerHTML += '\n\n------------------------------------------------------------------------'
         if (valgtAbonoment[0]) {
+            //kjører dersom et abonoment er huket av 
             output.innerHTML += '\n\nMånedlig\n';
             output.innerHTML += valgtAbonoment[0] + ": kr " + valgtAbonoment[1] + ',- \n' + aRabattString;
             totalpris += parseFloat(valgtAbonoment[1]) * aRabatt;
             abnonomentString = " \nmed en månedlig regning på kr " + twoDecimal(valgtAbonoment[1] * aRabatt) + ",-"
         }
         if (valgteEngagskoster[0]) {
+            //kjører om en engangskost er huket av
             output.innerHTML += "\n\nEngangspriser"
-            for (let element of valgteEngagskoster) {
-                console.log(element.navn, element.pris);
-                output.innerHTML += "\n"+element.navn + ': kr ' + element.pris + ',-';
+            for (let element of valgteEngagskoster) { // ser hvilke som er huket av
+                output.innerHTML += "\n" + element.navn + ': kr ' + element.pris + ',-';
                 totalEngangspriser += parseFloat(element.pris) * eRabatt;
                 if (eRabatt != 1) {
+                    //kjører kode hvis det er rabatt
                     minusERabatt = Math.round((parseFloat(element.pris) - parseFloat(element.pris) * eRabatt) * 100) / 100;
                     output.innerHTML += "\nRabatt: kr " + minusERabatt + ",-";
                 }
             }
+            //legger til i totalpris
             totalpris += totalEngangspriser;
         }
 
-
+        //skriver ut
         abonomentString = "med en månedlig regning på kr " + twoDecimal(valgtAbonoment[1] * aRabatt) + ",-";
         output.innerHTML += "\n\nTotalpris blir kr " + twoDecimal(totalpris) + ",-" + abonomentString + ".";
         output.innerHTML += '\n\n------------------------------------------------------------------------'
         output.innerHTML += "\n\nDet er bare å svare på denne eposten hvis du har noen spørsmål. \n\nMed vennlig hilsen " + formEl.agent.value;
-        // output.innerHTML += '\n\n'+valgtAbonoment[0];
-        // output.innerHTML += '\n\n'+valgtAbonoment[0];
 
     } else {
+        //case 2: ingenting er krysset av
         output.innerHTML = "Huk av minst ett felt.";
     }
 
 
 
 })
-
+//runder av til 2 desimaler
 function twoDecimal(num) {
-    return Math.round(num*100)/100
+    return Math.round(num * 100) / 100
+}
+//funksjon som legger alle feltene på nettsiden
+function printInput(tittel, array, name, inputType) {
+    inputDiv.innerHTML += "<h3> " + tittel + "</h3>";
+    for (let service of array) {
+        inputDiv.innerHTML += "<input name = '" + name + "' type = '" + inputType + "' readonly value = '" + service.navn + "|" + service.pris + "' > <label>" + service.navn + "</label> <br>";
+    }
 }
---
+++ uke 1/4- fredag/script.js
@@ -0,0 +1,40 @@
+let formEl = document.getElementById("form");
+let rørSekk = { vekt: 25, liter: 12.5, kost: 89 };
+let outputEl = document.getElementById("output");
+
+formEl.addEventListener("submit", function (e) {
+    e.preventDefault();
+    if(formEl.yDiameter.value > formEl.iDiameter.value) {
+        //gir output
+        regnAntallPoser(cmTilLiter(regnRørVolumn()), rørSekk.liter, rørSekk.kost);
+    } else {
+        //feilmelding
+        outputEl.innerHTML = "Ytre diameter må være større enn indre diameter."
+    }
+})
+function regnVolum(diameter, lengde) {
+    //regner volumet til en sylinder
+    return (diameter / 2) ** 2 * lengde * Math.PI;
+}
+function regnRørVolumn() {
+    //finner differansen mellom sylynden sitt ytre og indre volum
+    return regnVolum(form.yDiameter.value, form.lengde.value) - regnVolum(form.iDiameter.value, form.lengde.value);
+}
+function cmTilLiter(kubikkCm) {
+    //gjør om til liter
+    return kubikkCm / 1000;
+}
+function regnAntallPoser(volum, liter, pris) {
+    //regner antall poser
+    poser = Math.ceil(volum/liter);
+    //output
+    switch (poser) {
+        case 1:
+            outputEl.innerHTML = "Du må kjøpe " + poser + " pose. Det vil koste deg kr " + pris * poser + ",-.";
+            break;
+        default:
+            outputEl.innerHTML = "Du må kjøpe " + poser + " poser. Det vil koste deg kr " + pris * poser + ",-.";
+            break;
+    }
+
+}
